7ee949eeb8ee39c8ba9af022e5dd8f089a00b224
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-20 13:27:47 +1030

    add function to play game 5 timesand as many times the user wants

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+var wins = 0;
+function count() {
+  wins += 1;
+  return wins;
+}
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "scissor") {
     console.log("player wins");
+    count();
   } else if (playerSelection == "paper" && computerSelection == "rock") {
     console.log("player wins");
+    count();
   } else if (playerSelection == "scissor" && computerSelection == "paper") {
     console.log("player wins");
+    count();
   } else if (playerSelection == computerSelection) {
     console.log("it's a tie");
   } else {
@@ -15,10 +23,33 @@ choices = ["rock", "paper", "scissor"];
 
 function game() {
   var computerChoice = choices[Math.floor(Math.random() * 3)];
-  var playerChoice = prompt("please enter rock, paper or scissor");
+  console.log(computerChoice);
+  var playerChoice = prompt(
+    "please enter rock, paper or scissor"
+  ).toLowerCase();
   playRound(playerChoice, computerChoice);
 }
-
 for (i = 0; i < 5; i++) {
   game();
+}
+prompt(`total player wins is ${wins} and computer wins ${5 - wins}`);
+
+function playAgainCommand() {
+  for (i = 0; i < 5; i++) {
+    game();
+  }
+  prompt(`total player wins is ${wins} and computer wins ${5 - wins}`);
+  var playAgain = prompt("play again? (Y/N) or (y/n)").toLowerCase();
+  if (playAgain == "y") {
+    playAgainCommand();
+  } else {
+    prompt("Thankyou for playing");
+  }
+}
+
+var playAgain = prompt("play again? (Y/N) or (y/n)").toLowerCase();
+if (playAgain == "y") {
+  playAgainCommand();
+} else {
+  prompt("Thankyou for playing");
 }
